469965d9a3fb59f40417368e57e8999659bcfc7e
silas.lachauer <lachauersilas@gmail.com>
2025-08-18 09:38:07 +0200

    Projektdateien hinzufügen.

---
+++ M320/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace M320
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 2;
+            int b = 3;
+            int summe = my_math.Add (a, b);
+            Console.WriteLine($"Summe von {a} und {b} = {summe}");
+        }
+    }
+}
