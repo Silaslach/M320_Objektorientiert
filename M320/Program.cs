b91b484cc4cf4f6cfbea12378205832953d406b2
silas.lachauer <lachauersilas@gmail.com>
2025-08-18 09:47:45 +0200

    mal in txt

--- M320/Program.cs
+++ M320/Program.cs
@@ -8,7 +8,7 @@
             int a = 2;
             int b = 3;
             int summe = my_math.Add (a, b);
-            Console.WriteLine($"Summe von {a} und {b} = {summe}");
+            Console.WriteLine($"Summe von {a} und {b} mal {b} = {summe}");
         }
     }
 }
